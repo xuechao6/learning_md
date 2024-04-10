70cf7f21fc74588f4c4339357b02a75d7d5742a2
xuechao6 <1254554188@qq.com>
2024-04-10 12:13:26 +0800

    add

---
+++ track.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct position
+{
+    double lon;
+    double lat;
+    double alt;
+};
+
+void track_point(double dis_ , double angle_ , position p_tar , position& p_exp)
+{
+    cout<<"hello"<<endl;
+}
+
+int main()
+{
+    cout<<"hello"<<endl;
+    return 0;
+}
